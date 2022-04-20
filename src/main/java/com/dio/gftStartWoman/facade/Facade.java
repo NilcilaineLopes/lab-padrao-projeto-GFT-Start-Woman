b1ba124e706cc773a0b2ef20c40249abe0090431
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-20 17:04:25 -0300

    Implemnta Simgleton

--- src/main/java/com/dio/gftStartWoman/facade/Facade.java
+++ src/main/java/com/dio/gftStartWoman/facade/Facade.java
@@ -1,7 +1,13 @@
 package com.dio.gftStartWoman.facade;
 
-public class Facade {
-    public void migrarCliente(String nome, String cpf){
+import com.dio.gftStartWoman.subsistema1.crm.CrmService;
+import com.dio.gftStartWoman.subsistema2.cep.CepApi;
 
+public class Facade {
+    public void migrarCliente(String nome, String cep){
+        String cidade = CepApi.getInstacia().recuperarCidade(cep);
+        String estado = CepApi.getInstacia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
     }
 }
