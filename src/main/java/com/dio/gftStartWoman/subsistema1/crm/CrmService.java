fc02cac647b10ed4662628b57b02eb7ecf390663
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-04-20 17:01:01 -0300

    Mostra informação de um cliente no sistema CRM

--- src/main/java/com/dio/gftStartWoman/subsistema1/crm/CrmService.java
+++ src/main/java/com/dio/gftStartWoman/subsistema1/crm/CrmService.java
@@ -6,7 +6,11 @@ public class CrmService {
         super();
     }
 
-    public static void gravarCliente(String nome, String cep,  String estacidade, String estado) {
-        System.out.println("Cliente salvo no sistema d CRM.");
+    public static void gravarCliente(String nome, String cep,  String cidade, String estado) {
+        System.out.println("Cliente salvo no sistema d CRM:");
+        System.out.println(nome);
+        System.out.println(cep);
+        System.out.println(cidade);
+        System.out.println(estado);
     }
 }
